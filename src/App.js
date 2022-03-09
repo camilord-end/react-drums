80a4f8c2547bcfb01633a201f353b7c692f0007b
camilord-end <jcorozcoa@correo.udistrital.edu.co>
2022-03-09 12:23:18 -0500

    Templates

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import Drums from "./components/Drums";
+import Text from "./components/Text";
+import "./styles/App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h3>React Drums Machine</h3>
       </header>
+      <div className="Content">
+        <Drums />
+        <Text />
+      </div>
     </div>
   );
 }
